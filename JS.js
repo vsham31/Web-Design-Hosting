9ab4e1f32c56088f5c28c513e338a6fb38d51ccb
Vaibhhav Sharma <52786476+vsham31@users.noreply.github.com>
2022-01-31 13:56:50 +0530

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,12 @@
+// document ready
+(function ($) {
+
+    // scroll functions
+    $(window).scroll(function(e) {
+    
+        // add/remove class to navbar when scrolling to hide/show
+        $('.navbar')[$(window).scrollTop() >= 150 ? 'addClass' : 'removeClass']('navbar-hide');
+    
+    });
+    
+})(jQuery);    
